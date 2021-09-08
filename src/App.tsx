2dc2b8c21bd282e29317ec5544bb3d5b07382c6f
Misho <mimukh18@freeuni.edu.ge>
2021-09-08 11:10:19 +0400

    Electron support

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import MainPage from "./page/MainPage";
 
-function App() {
+const App = () => {
+  console.log("App.js");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact component={MainPage} path="/" />
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
